5abd7006e78330f14565985aec8e74da0f4c8519
Manuela <manueladeruvo2@gmail.com>
2022-06-08 11:43:12 +0200

    committo java

---
+++ hello.java
@@ -0,0 +1,26 @@
+package Main;
+import java.util.Arrays;
+public class Esercizio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {4,9,3,7};
+		System.out.println("il nuovo array è:" +Arrays.toString(shiftItem(nums)));
+		
+		public static int[] shiftItem(int[]nums) {
+			int[] shifter = new int[nums.length];
+			int n = nums.length;
+			for(int i=0;i<nums.lenght;i++) {
+				if (i== nums.length-1)
+					shifted[0] = nums[i];
+				else
+					
+				
+			}
+			system.out.println(int[]nums);
+			
+		}
+ 
+	}
+
+}
